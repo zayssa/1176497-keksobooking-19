e1aa0a0f3947a057f47c9811f250efcbf84f700a
Marina Petrova <zayssa@mail.ru>
2020-03-09 16:36:33 +0300

    7-3

--- js/mapfilter.js
+++ js/mapfilter.js
@@ -1,6 +1,21 @@
 'use strict';
 
 (function () {
+  var inputs = {
+    type: document.querySelector('#housing-type'),
+    price: document.querySelector('#housing-price'),
+    rooms: document.querySelector('#housing-rooms'),
+    guests: document.querySelector('#housing-guests'),
+    wifi: document.querySelector('#filter-wifi'),
+    dishwasher: document.querySelector('#filter-dishwasher'),
+    parking: document.querySelector('#filter-parking'),
+    washer: document.querySelector('#filter-washer'),
+    elevator: document.querySelector('#filter-elevator'),
+    conditioner: document.querySelector('#filter-conditioner')
+  };
+
+  var keys = Object.keys(inputs);
+
   var switchDisable = function (flag) {
     var formsList = document.querySelectorAll('.map__filters');
     for (var i = 0; i < formsList.length; i++) {
@@ -11,16 +26,38 @@
     }
   };
 
-  var type = document.querySelector('#housing-type');
+  var exportValues = function () {
+    window.mapfilter.type = inputs.type.value;
+    window.mapfilter.price = inputs.price.value;
+    window.mapfilter.rooms = inputs.rooms.value;
+    window.mapfilter.guests = inputs.guests.value;
 
-  type.addEventListener('change', function () {
-    window.mapfilter.type = type.value;
+    window.mapfilter.wifi = inputs.wifi.checked;
+    window.mapfilter.dishwasher = inputs.dishwasher.checked;
+    window.mapfilter.parking = inputs.parking.checked;
+    window.mapfilter.washer = inputs.washer.checked;
+    window.mapfilter.elevator = inputs.elevator.checked;
+    window.mapfilter.conditioner = inputs.conditioner.checked;
+  };
 
-    window.map.fill();
-  });
+  var debounceTO;
+  var debounce = function () {
+    clearTimeout(debounceTO);
+    debounceTO = setTimeout(function () {
+      exportValues();
+
+      window.map.fill();
+    }, 500);
+  };
+
+  for (var i = 0; i < keys.length; i++) {
+    inputs[keys[i]].addEventListener('change', function () {
+      debounce();
+    });
+  }
 
   window.mapfilter = {
-    switchDisable: switchDisable,
-    type: type.value
+    switchDisable: switchDisable
   };
+  exportValues();
 })();
